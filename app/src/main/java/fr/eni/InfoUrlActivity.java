0b8aec10caafc77228bca6c07d644ae9f35625da
Pierre Heydon <pierre.heydon@laposte.net>
2022-04-28 12:08:21 +0200

    add webview url article

--- app/src/main/java/fr/eni/InfoUrlActivity.java
+++ app/src/main/java/fr/eni/InfoUrlActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import fr.eni.bo.Article;
 
 public class InfoUrlActivity extends AppCompatActivity {
 
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +27,34 @@ public class InfoUrlActivity extends AppCompatActivity {
         // Recup de l'extra article
         Article article = intent.getParcelableExtra("article");
 
-        TextView url = findViewById(R.id.tv_url);
+        TextView url = findViewById(R.id.infoUrl_tv_url);
         url.setText(article.getUrl());
 
-        Log.i("toto", "Article : " + article.getNom() + " url : " + article.getUrl());
+         Log.i("toto", "InfoUrlActivity / onCreate / article : " + article.getNom() + " url : " + article.getUrl());
 
-        // Toast.makeText(this, article.getUrl(), Toast.LENGTH_SHORT).show();
 
+        webView = findViewById(R.id.infoUrl_wv);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl(article.getUrl());
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Intercepté le click sur retour pour le répércuter sur la webview
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
 
